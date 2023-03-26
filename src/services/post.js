ee453935ab1520ca32b8d2f238f879793937c76b
ztlee042 <leeemail9898@gmail.com>
2023-03-26 23:26:19 +0200

    Update posts service

--- src/services/post.js
+++ src/services/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // const baseUrl = 'http://localhost:9000/posts'
-const baseUrl = 'http://localhost:9000/posts'
+const baseUrl = 'http://localhost:3001/posts'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
@@ -8,8 +8,8 @@ const getAll = async () => {
 }
 
 const getOne = async (id) => {
-  //const returnedPost = await axios.get(`${baseUrl}/${id}`)
-  const returnedPost = await axios.get(`http://localhost:3000/db.json`)
+  const returnedPost = await axios.get(`${baseUrl}/${id}`)
+  // const returnedPost = await axios.get(`http://localhost:3000/db.json`)
   return returnedPost.data
 }
 
