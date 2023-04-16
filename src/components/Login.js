ffd8824052b77dc6885f6fd1a66af04e0a019e41
QiuZeHang <zehqiu@student.ethz.ch>
2023-04-16 13:07:27 +0200

    update files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Box} from "@mui/material";
 import LoginForm from "./LoginComponents/LoginForm";
-import logo from "./LoginComponents/logo512.png";
+import { LOGO_API } from "../services/APIs";
 import "./LoginComponents/Login.css";
 
 function Login() {
@@ -9,7 +9,7 @@ function Login() {
   return (
     <Box className="Loginpage-container">
       <img
-          src={logo}
+          src={LOGO_API}
           alt="Godzilla logo"
           style={{ width: "100px", // Set the width to your desired size
           height: "auto", // Set the height to "auto" to maintain aspect ratiomarginBottom: "20px" 
--- src/services/APIs.js
+++ src/services/APIs.js
@@ -4,3 +4,5 @@ export const LOGIN_API = "/api/login";
 // Add other API URLs as needed
 
 export const SIGNUP_API = "/api/signup";
+
+export const LOGO_API = "/api/logo";
